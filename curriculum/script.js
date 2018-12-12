18a2ecf9aed4e80274fcfec310a3999776fa4d11
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-12-12 07:09:44 -0200

    add functions javascript pt/br

--- curriculum/script.js
+++ curriculum/script.js
@@ -1,7 +1,26 @@
-function enableStylesheet (node) {
-  node.media = '';
-}
+  const en = document.querySelector('#en-us');
+  const pt = document.querySelector('#pt-br');
+  enableStylesheet(en);
 
-function disableStylesheet (node) {
-  node.media = 'none';
-}
+  function enableStylesheet (node) {
+    node.media = '';
+  }
+
+  function disableStylesheet (node) {
+    node.media = 'none';
+  }
+  function enablePt () {
+    console.log('pt');
+    enableStylesheet(pt);
+    disableStylesheet(en);
+  }
+
+  function enableEn () {
+    console.log('en');
+    enableStylesheet(en);
+    disableStylesheet(pt);
+  }
+
+
+  document.querySelector("#pt").onclick = enablePt;
+  document.querySelector("#en").onclick = enableEn;
